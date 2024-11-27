56cc855d0564515c41b11548419fe4d436ca8907
radi <miahradi@gmail.com>
2024-11-27 20:44:45 +0100

    changed so that footer changes with screen

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footer = document.querySelector('.footer');
-    const bodyHeight = document.documentElement.scrollHeight;
-    const viewportHeight = window.innerHeight;
 
-    if (bodyHeight <= viewportHeight) {
-        footer.style.position = 'absolute';
-    } else {
-        footer.style.position = 'relative';
+    function adjustFooter() {
+        const bodyHeight = document.documentElement.scrollHeight;
+        const viewportHeight = window.innerHeight;
 
+        if (bodyHeight <= viewportHeight) {
+            footer.style.position = 'absolute';
+        } else {
+            footer.style.position = 'relative';
+        }
+
+        footer.style.left = '0';
+        footer.style.bottom = '0';
     }
 
-    footer.style.left = '0';
-    footer.style.bottom = '0';
+    window.addEventListener('resize', adjustFooter);
+    adjustFooter();
 });
