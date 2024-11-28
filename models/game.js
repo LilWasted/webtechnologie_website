86a01f1933da457495cb810502b7ab5ce433ecc3
radi <miahradi@gmail.com>
2024-11-28 15:28:12 +0100

    games yt link

--- models/game.js
+++ models/game.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const GameSchema = new Schema(
-    {name : { type: String, min: 1, max: 100, required: true },});
-
-// Virtual for book's URL
-GameSchema.virtual("url").get(function () {
-    // We don't use an arrow function as we'll need this object
-    return `/home/games/${this._id}`;
+const GameSchema = new Schema({
+    name : { type: String, min: 1, max: 100, required: true },
+    YTlink : { type: String, max: 100, default: ""}
 });
 
 // Virtual for book's URL
